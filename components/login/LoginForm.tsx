7fab0b0c907db1ead99d6b3a36b0a64305c67c39
Louis Cavecchi <cavecchilouis@gmail.com>
2023-02-20 14:22:58 +0100

    [HACKATHON][FIX]:Add concurrently dependency

--- components/login/LoginForm.tsx
+++ components/login/LoginForm.tsx
@@ -1,8 +1,10 @@
 import PrimaryButton from "@/components/buttons/PrimaryButton";
+import TextInput from "@/components/inputs/TextInput";
 
 export default function LoginForm(){
     return (
         <form>
+            <TextInput name="email" placeholder="Enter name"></TextInput>
             <PrimaryButton text="Connexion" type="submit"></PrimaryButton>
         </form>
     )
