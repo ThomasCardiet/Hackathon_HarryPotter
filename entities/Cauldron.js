30426dc4ba4284005ae67ccb2c6c5edd96a4e3d9
Thomas Cardiet <thom_car@yahoo.fr>
2023-02-20 11:52:07 +0100

    fix: move entities helpers directly to Entity class file

--- entities/Cauldron.js
+++ entities/Cauldron.js
@@ -1,4 +1,4 @@
-import { CauldronType } from './CauldronType';
+import { CauldronType, CAULDRON_TYPES } from './CauldronType';
 
 export class Cauldron {
   type;
@@ -36,3 +36,38 @@ export class Cauldron {
     this.type = type;
   }
 }
+
+// CAULDRONS LIST
+export const CAULDRONS = {
+  FOLDABLE: new Cauldron(CAULDRON_TYPES.FOLDABLE),
+  SILVER: new Cauldron(CAULDRON_TYPES.SILVER),
+  COPPER: new Cauldron(CAULDRON_TYPES.COPPER),
+  BRASS: new Cauldron(CAULDRON_TYPES.BRASS),
+};
+
+/**
+ * TRANSFORM CAULDRONS TO ARRAY
+ */
+export const getCauldrons = () => {
+  return Object.values(CAULDRONS);
+};
+
+/**
+ * GET CAULDRON BY NAME
+ *
+ * @param {String} name
+ *
+ */
+export const getCauldronByName = (name) => {
+  return getCauldrons().find((cauldron) => cauldron.getName() === name);
+};
+
+/**
+ * GET CAULDRON BY TYPE
+ *
+ * @param {CauldronType} type
+ *
+ */
+export const getCauldronByType = (type) => {
+  return getCauldrons().find((cauldron) => cauldron.getType() === type);
+};
