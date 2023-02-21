b7b30038beaeeafad25a6f2679f394f7bb0b685f
AxelHuon <81225848+AxelHuon@users.noreply.github.com>
2023-02-21 10:58:55 +0100

    [HACKATHON][FIX]:fix style

---
+++ pages/home.tsx
@@ -0,0 +1,18 @@
+
+export default function Home () {
+    return (
+        <section className={"home"}>
+            <aside className={"home-container"}>
+                <div className={"home-container-title"}>
+                    <h1 className={"text-100 text-Harry text-yellow"}>Harry Potion</h1>
+                </div>
+                <div className={"home-container-description"}>
+                    <p className={"text-15 text-white text-ProzaLibre-Regular"}>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Beatae consectetur deleniti ea enim, eos in inventore molestiae nihil officia perferendis.</p>
+                </div>
+                <div className={"home-container-button"}>
+                    <button className={"btn-reset btn-yellow"}>Commencer</button>
+                </div>
+            </aside>
+        </section>
+    )
+}
