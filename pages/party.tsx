65378d834c6a11303c92e44030d5dacb60d0dcb7
AxelHuon <81225848+AxelHuon@users.noreply.github.com>
2023-02-22 11:57:48 +0100

    [HACKATHON-desing/game][feature]: system indice card

--- pages/party.tsx
+++ pages/party.tsx
@@ -3,8 +3,8 @@ import { DraggableIngredient } from '@/components/DraggableIngredient'
 import { DropBlock } from '@/components/DropBlock'
 import { getShuffledIngredients, Ingredient } from '@/entities/Ingredient'
 import { getCauldrons } from '@/entities/Cauldron'
-import { useEffect, useState } from 'react';
-
+import {useEffect, useRef, useState} from 'react';
+import {gsap} from "gsap";
 const Party = () => {
     const [ingredients, setIngredients] = useState([])
     const cauldrons = getCauldrons();
@@ -19,7 +19,55 @@ const Party = () => {
      */
     const dropIngredient = (droppedIngredient: Ingredient) => {
       setIngredients(ingredients.filter((ingredient: Ingredient) => droppedIngredient !== ingredient))
+        setCanTakeIndice(true)
     }
+
+
+    const [indices, setIndices] = useState([{img :"" , indiceTaken : false},{img :"" , indiceTaken : false},{img :"" , indiceTaken : false}]);
+    const [classFlipFirst, setClassFlipFirst] = useState("");
+    const [classFlipSecond, setClassFlipSecond] = useState("");
+    const [classFlipThird, setClassFlipThird] = useState("");
+    const [canTakeIndice, setCanTakeIndice] = useState(true);
+
+    const launchIndices = (index:number)=>{
+        let findIndice = indices.find((item,indexItem)=>indexItem === index)
+        if (findIndice.indiceTaken === false && canTakeIndice){
+            /*Set to true if already taken*/
+            let indicesCopy = [...indices]
+            findIndice.indiceTaken = true
+            console.log(findIndice)
+            setIndices(indicesCopy)
+            if (index === 0){
+                setClassFlipFirst("flip")
+            }else if (index === 1){
+                setClassFlipSecond("flip")
+            }else if (index === 2){
+                setClassFlipThird("flip")
+            }
+            setCanTakeIndice(false)
+            setTimeout(() => {
+                setClassFlipSecond("")
+                setClassFlipFirst("")
+                setClassFlipThird("")
+            }, "5000")
+
+        }
+    }
+
+    useEffect(() => {
+        console.log(indices)
+    }, [indices]);
+
+
+
+
+
+    useEffect(() => {
+        console.log(indices)
+    }, [indices]);
+
+
+
 
     return (
       <>
@@ -28,9 +76,53 @@ const Party = () => {
         <meta name="description" content="Harry Potter potion maker game" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
       </Head>
-      
+
       <main>
         <DropBlock cauldron={cauldrons[0]} setDropBoxOffsets={setDropBoxOffsets} />
+          <div className={"indices"}>
+              <div className={"indices-container"}>
+                  {indices.map((item,index)=>{
+                      if (index === 0){
+                          return(
+                              <div onClick={(e)=>launchIndices(index)} key={index} className={"indices-container-item first " + (classFlipFirst) + (canTakeIndice ? "" : " inactive") + (item.indiceTaken === true ? " inactive" : "")}>
+                                  <div className={"indices-container-item-image"}>
+                                  </div>
+                                  <div className={"indices-container-item-background "}>
+                                      <img src={"images/carte.png"}/>
+                                  </div>
+                                  <div className={"content"}>
+                                      <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aut commodi consequatur facere impedit nobis perspiciatis, quam quasi sunt ullam veniam.</p>
+                                  </div>
+                              </div>
+                          )
+                      }else if (index ===1)
+                      {
+                          return(
+                              <div onClick={(e)=>launchIndices(index)} key={index} className={"indices-container-item second " + (classFlipSecond) + (canTakeIndice ? "" : " inactive")+ (item.indiceTaken === true ? " inactive" : "")}>
+                                  <div className={"indices-container-item-image"}>
+
+                                  </div>
+                                  <div className={"indices-container-item-background"}>
+                                      <img src={"images/carte.png"}/>
+                                  </div>
+                              </div>
+                          )
+                      }else if (index ===2)
+                      {
+                          return(
+                              <div onClick={(e)=>launchIndices(index)} key={index} className={"indices-container-item third " + (classFlipThird) + (canTakeIndice ? "" : " inactive")+ (item.indiceTaken === true ? " inactive" : "")}>
+                                  <div className={"indices-container-item-image"}>
+
+                                  </div>
+                                  <div className={"indices-container-item-background"}>
+                                      <img src={"images/carte.png"}/>
+                                  </div>
+                              </div>
+                          )
+                      }
+                  })}
+              </div>
+          </div>
         <div className="ingredients-block">
           {ingredients.map((ingredient, index) => (<DraggableIngredient dropBoxOffsets={dropBoxOffsets} ingredient={ingredient} dropIngredient={dropIngredient} key={index} />
           ))}
