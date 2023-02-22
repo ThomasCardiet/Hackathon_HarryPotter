4be5a92f6ccfecd77a181f6dc3e455c3acd93387
Thomas Cardiet <thom_car@yahoo.fr>
2023-02-22 09:35:15 +0100

    feat: add socket and add routing

---
+++ router/index.js
@@ -0,0 +1,65 @@
+export class Router {
+  // ROUTES
+
+  /**
+   * @param {String} name
+   * @param {String} slug
+   */
+  static routes = {
+    CHOICE: {
+      name: 'Choice',
+      slug: '/choice',
+    },
+    LOGIN: {
+      name: 'Login',
+      slug: '/login',
+    },
+    HOME: {
+      name: 'Home',
+      slug: '/',
+    },
+    PARTY: {
+      name: 'Party',
+      slug: '/party',
+    },
+  };
+
+  /**
+   * GET ROUTES
+   *
+   * @returns {Object}
+   */
+  static getRoutes() {
+    return this.routes;
+  }
+
+  /**
+   * GET STATIC PATHS FOR ROUTING
+   *
+   * @returns {Array<Object>}
+   */
+  static getStaticPaths(param = 'slug') {
+    return Object.values(this.getRoutes()).map((route) => ({
+      params: { [param]: route.slug.substring(1).split('/') },
+    }));
+  }
+
+  /**
+   * GET PATH BY ARRAY OF PARAMS
+   *
+   * @returns {String}
+   */
+  static getPathByArray(params) {
+    return params && params.length > 0 ? `/${params.join('/')}` : '/';
+  }
+
+  /**
+   * GET ROUTE BY SLUG
+   * @param {String} slug
+   *
+   * @returns {Object}
+   */
+  static getRouteBySlug(slug) {
+    return Object.values(this.getRoutes()).find((route) => route.slug === slug);
+  }
+}
