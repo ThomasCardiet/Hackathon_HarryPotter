f3d2b1703fc668c8c1b36bed58f46cc88cb3aa69
Sofian Slimani <70762272+Sev7n2507@users.noreply.github.com>
2023-02-22 10:40:04 +0100

    fix: update Choice page style

--- components/Choice/index.js
+++ components/Choice/index.js
@@ -14,16 +14,25 @@ const Choice = () => {
       <main>
         <section className={'choice'}>
           <aside className={'choice-container'}>
+            <div className={'choice-container-user'}>
+              <div className={'choice-container-user-image'}>
+                <img src={'images/houses/serpentard.png'} alt={'house'} />
+              </div>
+
+              <h2 className={'text-20 text-ProzaLibre-SemiBold text-white'}>
+                Axel
+              </h2>
+            </div>
             <div className={'choice-container-title'}>
               <h1 className={'text-50 text-ProzaLibre-SemiBold text-white'}>
                 Make your choice
               </h1>
             </div>
             <div className={'choice-container-button'}>
-              <Link href={'/create'} className={'btn-reset btn-white'}>
+              <Link href={'/create'} className={'btn-reset btn-yellow'}>
                 Create a party
               </Link>
-              <Link href={'/join'} className={'btn-reset btn-white'}>
+              <Link href={'/join'} className={'btn-reset btn-yellow'}>
                 Join a party
               </Link>
             </div>
