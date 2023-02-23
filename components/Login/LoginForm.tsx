5c4d332f7986ab4321e939396df797907cc784f0
Thomas Cardiet <thom_car@yahoo.fr>
2023-02-23 10:27:44 +0100

    feat: setup rooms

--- components/Login/LoginForm.tsx
+++ components/Login/LoginForm.tsx
@@ -45,8 +45,7 @@ export default function LoginForm({setUser}){
           })
         }else if (!data.statusCode){
         Api.storeUser(data);
-        setUser(data.user)
-          console.log(data)
+        setUser(data.user);
         router.push(Router.getRoutes().CHOICE.slug)
         return toast.success("Connexion réussie", {
           icon : "🧙",
